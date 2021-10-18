d221d6c9473e5e23a33040085a1f97160d99f50d
CySaLa <78034751+CySaLa@users.noreply.github.com>
2021-10-19 00:02:09 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,13 @@
+console.log("Merhaba Kodlama.io")
+
+var dolarDun = 9.20
+var dolarBugun = 9.30
+
+console.log(dolarDuN)
+
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi"]
+console.log("<ul>")
+for(let i = 0;i<konutKredileri.length;i++){
+    console.log("<li>"+i+"</li>")
+}
+console.log("</ul>")
